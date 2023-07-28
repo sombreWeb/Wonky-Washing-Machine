f8f24dd977e09768325d4aa4087ee4b658d9a710
sombreWeb <73797829+sombreWeb@users.noreply.github.com>
2023-07-28 12:00:26 +0100

    Updated calibration functions for wires

--- src/Wonky-Washing-Machine/wires.ino
+++ src/Wonky-Washing-Machine/wires.ino
@@ -10,7 +10,7 @@ float Vin = 5;                      // Input voltage
 float Rref = 20000;                    // Reference resistor's value in ohms
 
 int homePort = 0;
-float redPortOffsets[numSensors] = {0, -0.033, -0.025, -0.02};
+float redPortOffsets[numSensors] = {0, -0.0334, -0.0255, -0.0196};
 
 int numberOfResistanceTests = 100;
 
@@ -39,13 +39,16 @@ bool compareByColourAndRedPort(const wireConnection& wire1, const wireConnection
   }
 }
 
+bool compareByResistance(const wireConnection& wire1, const wireConnection& wire2) {
+    return wire1.resistanceValue < wire2.resistanceValue;
+}
+
 double calculatePercentageDifference(double value1, double value2) {
 
-  if (value1 == value2) {
-    return 0.0;  // To avoid division by zero
-  }
-
   double percentageDiff = ((value1 - value2) / ((value1 + value2) / 2.0)) * 100.0;
+  if (isnan(percentageDiff)) {
+    return 0.0;
+  }
   return percentageDiff;
 }
 
@@ -86,16 +89,41 @@ std::vector<wireConnection> readCurrentWireSetup(int redOffset, int blackOffset,
   return currentWireSetup;
 }
 
+void printWireConnections(std::vector<wireConnection> values) {
+  for (const auto& connection : values) {
+    Serial.print("Resistance: ");
+    Serial.print(connection.resistanceValue);
+    Serial.print(" Ohms, Red Port: ");
+    Serial.print(connection.redPort);
+    Serial.print(", Colour: ");
+    Serial.print(connection.colour);
+    Serial.print(", Black Port: ");
+    Serial.println(connection.blackPort);
+  }
+  Serial.println("---------------");
+}
+
+void printCurrentWireConnections() {
+  std::vector<wireConnection> currentValues = readCurrentWireSetup(0, 0);
+  std::sort(currentValues.begin(), currentValues.end(), compareByColourAndBlackPort);
+  printWireConnections(currentValues);
+}
+
 void calibrateRedPorts() {
 
   std::vector<wireConnection> calibrationValues;
 
+  boolean firstRedCalibration = true;
   for (int moveRedWires = 0; moveRedWires < numSensors; moveRedWires++) {
-    std::vector<wireConnection> currentValues = readCurrentWireSetup(moveRedWires, 0, false);
-    calibrationValues.insert(calibrationValues.end(), currentValues.begin(), currentValues.end());
 
     do {
-      Serial.println("Move each red connection one to the right. Enter ok to continue.");
+      if (firstRedCalibration) {
+        Serial.println("Setup the red connections in the natural position RGBY from left to right. Enter ok to continue.");
+        firstRedCalibration = false;
+      } else {
+        Serial.println("Move each red connection one to the right. Enter ok to continue.");
+      }
+
       while (Serial.available() == 0) {
       }
 
@@ -108,9 +136,11 @@ void calibrateRedPorts() {
         Serial.println("Invalid input. Please enter 'ok'.");
       }
     } while (true);
-  }
 
-  allWireConnections = calibrationValues;
+
+    std::vector<wireConnection> currentValues = readCurrentWireSetup(moveRedWires, 0, false);
+    calibrationValues.insert(calibrationValues.end(), currentValues.begin(), currentValues.end());
+  }
 
   struct portTotalCount {
     int port;
@@ -135,30 +165,36 @@ void calibrateRedPorts() {
     }
   }
 
-
-  // Calculate and print the average percentage difference for each red port
-  for (const auto& entry : portPercentageAverages) {
-    int redPort = entry.port;
-    Serial.print("Red Port: ");
-    Serial.print(redPort);
-    Serial.print(", Average Percentage Difference: ");
-    double averagePercentageDiff = entry.total / entry.count;
-    Serial.println(averagePercentageDiff);
-
+  Serial.print("{");
+  for (int redPort = 0; redPort < numSensors; redPort++) {
+    double averagePercentageDiff = portPercentageAverages[redPort].total / portPercentageAverages[redPort].count * -1 / 100;
+    if (isnan(averagePercentageDiff)) {
+      Serial.print(0.0);
+    } else {
+      Serial.print(averagePercentageDiff, 4);
+    }
+    if (redPort < numSensors - 1) {
+      Serial.print(", ");
+    }
   }
-
+  Serial.println("};");
 }
 
 void calibrateWires() {
 
   std::vector<wireConnection> calibrationValues;
 
+  boolean firstBlackCalibration = true;
   for (int moveBlackWires = 0; moveBlackWires < numSensors; moveBlackWires++) {
-    std::vector<wireConnection> currentValues = readCurrentWireSetup(0, moveBlackWires);
-    calibrationValues.insert(calibrationValues.end(), currentValues.begin(), currentValues.end());
 
     do {
-      Serial.println("Move each black connection one to the right. Enter ok to continue.");
+      if (firstBlackCalibration) {
+        Serial.println("Setup the black connections in the natural position RGBY from left to right. Enter ok to continue.");
+        firstBlackCalibration = false;
+      } else {
+        Serial.println("Move each black connection one to the right. Enter ok to continue.");
+      }
+
       while (Serial.available() == 0) {
       }
 
@@ -171,9 +207,16 @@ void calibrateWires() {
         Serial.println("Invalid input. Please enter 'ok'.");
       }
     } while (true);
+
+    std::vector<wireConnection> currentValues = readCurrentWireSetup(0, moveBlackWires);
+    calibrationValues.insert(calibrationValues.end(), currentValues.begin(), currentValues.end());
   }
 
   allWireConnections = calibrationValues;
+
+  //std::sort(allWireConnections.begin(), allWireConnections.end(), compareByColourAndBlackPort);
+  std::sort(allWireConnections.begin(), allWireConnections.end(), compareByResistance);
+  printWireConnections(allWireConnections);
 }
 
 void setup()
@@ -199,20 +242,10 @@ void loop()
     Serial.print("Port 3: ");
     Serial.println(findAverageResistance(3, 1000));
   */
-  calibrateRedPorts();
-  //calibrateWires();
-  std::sort(allWireConnections.begin(), allWireConnections.end(), compareByColourAndBlackPort);
+  //calibrateRedPorts();
+  calibrateWires();
 
-  for (const auto& connection : allWireConnections) {
-    Serial.print("Resistance: ");
-    Serial.print(connection.resistanceValue);
-    Serial.print(" Ohms, Red Port: ");
-    Serial.print(connection.redPort);
-    Serial.print(", Colour: ");
-    Serial.print(connection.colour);
-    Serial.print(", Black Port: ");
-    Serial.println(connection.blackPort);
-  }
-  Serial.println("---------------");
+  //printCurrentWireConnections();
+
 
 }
